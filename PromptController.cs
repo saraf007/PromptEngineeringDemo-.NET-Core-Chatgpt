3f632ad49d7cbb35d3767f950d521a36376b1d15
Vikas Saraf <Vikas_Saraf@epam.com>
2024-06-28 12:23:28 +0530

    Add project files.

---
+++ PromptController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PromptEngineeringDemo
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromptController : ControllerBase
+    {
+        private readonly IPromptService _promptService;
+
+        public PromptController(IPromptService promptService)
+        {
+            _promptService = promptService; 
+        }
+
+        [HttpGet("TriggerOpenAI")]
+        public async Task<IActionResult> TriggerOpenAI([FromQuery] string prompt)
+        {
+            var res = await _promptService.TriggerOpenAI(prompt);
+            return Ok(res);
+        }
+    }
+}
---
+++ PromptService.cs
@@ -0,0 +1,60 @@
+﻿using PromptEngineeringDemo.DTOs;
+using System.Text;
+using System.Text.Json;
+
+namespace PromptEngineeringDemo
+{
+    public class PromptService : IPromptService
+    {
+        public readonly IConfiguration _configuration;
+
+        public PromptService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<string> TriggerOpenAI(string prompt)
+        {
+            var apiKey = _configuration.GetValue<string>("OpenAISettings:APIKey");
+            var baseUrl = _configuration.GetValue<string>("OpenAISettings:BaseUrl");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
+
+            var request = new OpenAIRequestDto
+            {
+                Model = "gpt-3.5-turbo",
+                Messages = new List<OpenAIMessageRequestDto>
+                {
+                    new OpenAIMessageRequestDto
+                    {
+                        Role = "user",
+                        Content = prompt
+                    }
+                },
+                MaxTokens = 100
+            };
+
+            var json = JsonSerializer.Serialize(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(baseUrl, content);
+            var responseJson = await response.Content.ReadAsStringAsync();
+            
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorResponse = JsonSerializer.Deserialize<OpenAIErrorResponseDto>(responseJson);
+                throw new System.Exception(errorResponse.Error.Message);
+            }
+
+            var data = JsonSerializer.Deserialize<OpenAIResponseDto>(responseJson);
+            var responseText = data.choices[0].message.content;
+
+            return responseText;
+        }
+    }
+
+    public interface IPromptService
+    {
+        Task<string> TriggerOpenAI(string prompt);
+    }
+}
